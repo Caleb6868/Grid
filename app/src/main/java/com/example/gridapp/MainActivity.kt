1b533e4d348cbbec365934c0242beba1ce05e883
Caleb6868 <145353064+Caleb6868@users.noreply.github.com>
2024-10-03 11:05:51 -0500

    started on invidivual card

--- app/src/main/java/com/example/gridapp/MainActivity.kt
+++ app/src/main/java/com/example/gridapp/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.privacysandbox.ads.adservices.topics.Topic
 import com.example.gridapp.ui.theme.GridAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,47 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GridAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CardCompose()
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CardCompose() {
+    Card (modifier = Modifier) {
+        Row {
+            Image(
+               painter = painterResource(R.drawable.tech),
+                contentDescription = null
+            )
+            Column {
+                Text(
+                    modifier = Modifier.padding(16.dp),
+                    text = "test here",
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Row {
+                    Image(
+                        modifier = Modifier.padding(horizontal = 8.dp),
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = "321"
+                    )
+                }
+
+            }
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    GridAppTheme {
-        Greeting("Android")
-    }
+    CardCompose()
 }
+
---
+++ app/src/main/java/com/example/gridapp/model/Topic.kt
@@ -0,0 +1,7 @@
+package com.example.gridapp.model
+
+data class Topic(
+    val titleResId: Int,
+    val count: Int,
+    val imageResId: Int
+)
